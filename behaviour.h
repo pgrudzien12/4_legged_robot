48503a3ddd053f81c33affb49d3eac972495616e
Paweł Grudzień <p.grudzien12@live.com>
2019-10-26 23:31:05 +0200

    moved common behaviours to base class

--- behaviour.h
+++ behaviour.h
@@ -12,8 +12,92 @@ protected:
         this->logger = logger;
     }
 
+    void setStablePose()
+    {
+        robot->GetLeg(1)->setDesiredAngles(90, 90, 90);
+        robot->GetLeg(2)->setDesiredAngles(90, 90, 90);
+        robot->GetLeg(3)->setDesiredAngles(90, 90, 90);
+        robot->GetLeg(4)->setDesiredAngles(90, 90, 90);
+    }
+
+    void setBalanceForward()
+    {
+        float q1 = 52;
+        float q2 = -25;
+        float q3 = 17;
+        robot->GetLeg(1)->setDesiredAngles(90 - q1, 90 + q2, 90 + q3);
+        robot->GetLeg(4)->setDesiredAngles(90 + q1, 90 - q2, 90 - q3);
+
+        q1 = 26;
+        q2 = -32;
+        q3 = -11;
+        robot->GetLeg(2)->setDesiredAngles(90 - q1, 90 - q2, 90 - q3);
+        robot->GetLeg(3)->setDesiredAngles(90 + q1, 90 + q2, 90 + q3);
+    }
+
+    void setBalanceBackwards()
+    {
+        float q1, q2, q3;
+        q1 = 26;
+        q2 = -32;
+        q3 = -11;
+        robot->GetLeg(1)->setDesiredAngles(90 + q1, 90 + q2, 90 + q3);
+        robot->GetLeg(4)->setDesiredAngles(90 - q1, 90 - q2, 90 - q3);
+
+        q1 = 52;
+        q2 = -25;
+        q3 = 17;
+        robot->GetLeg(2)->setDesiredAngles(90 + q1, 90 - q2, 90 - q3);
+        robot->GetLeg(3)->setDesiredAngles(90 - q1, 90 + q2, 90 + q3);
+    }
+
+    void setBalanceDown()
+    {
+        float q1, q2, q3;
+        q1 = 86;
+        q2 = -80;
+        q3 = 0;
+        robot->GetLeg(1)->setDesiredAngles(90 - q1, 90 + q2, 90 + q3);
+        robot->GetLeg(4)->setDesiredAngles(90 + q1, 90 - q2, 90 - q3);
+        robot->GetLeg(2)->setDesiredAngles(90 + q1, 90 - q2, 90 - q3);
+        robot->GetLeg(3)->setDesiredAngles(90 - q1, 90 + q2, 90 + q3);
+    }
+
+    
+    void setBalanceUp()
+    {
+        float q1, q2, q3;
+        q1 = -45;
+        q2 = 45;
+        q3 = 0;
+        robot->GetLeg(1)->setDesiredAngles(90 - q1, 90 + q2, 90 + q3);
+        robot->GetLeg(4)->setDesiredAngles(90 + q1, 90 - q2, 90 - q3);
+        robot->GetLeg(2)->setDesiredAngles(90 + q1, 90 - q2, 90 - q3);
+        robot->GetLeg(3)->setDesiredAngles(90 - q1, 90 + q2, 90 + q3);
+    }
+
+    bool gotDesiredPose()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (!robot->GetLeg(i + 1)->gotDesiredAngles())
+                return false;
+        }
+
+        return true;
+    }
+
+    void setSpeetTTF(float ttf)
+    {
+        for (int i = 1; i <= 4; i++)
+        {
+            robot->GetLeg(i)->setSpeedTTF(ttf);
+        }
+    }
+
 public:
     virtual void update(long timeElapsed) = 0;
+
 
 protected:
     Robot *robot;
--- behaviour/balance.h
+++ behaviour/balance.h
@@ -11,88 +11,7 @@ public:
     }
 
 private: // functions
-    void setStablePose()
-    {
-        robot->GetLeg(1)->setDesiredAngles(90, 90, 90);
-        robot->GetLeg(2)->setDesiredAngles(90, 90, 90);
-        robot->GetLeg(3)->setDesiredAngles(90, 90, 90);
-        robot->GetLeg(4)->setDesiredAngles(90, 90, 90);
-    }
 
-    void setBalanceForward()
-    {
-        float q1 = 52;
-        float q2 = -25;
-        float q3 = 17;
-        robot->GetLeg(1)->setDesiredAngles(90 - q1, 90 + q2, 90 + q3);
-        robot->GetLeg(4)->setDesiredAngles(90 + q1, 90 - q2, 90 - q3);
-
-        q1 = 26;
-        q2 = -32;
-        q3 = -11;
-        robot->GetLeg(2)->setDesiredAngles(90 - q1, 90 - q2, 90 - q3);
-        robot->GetLeg(3)->setDesiredAngles(90 + q1, 90 + q2, 90 + q3);
-    }
-
-    void setBalanceBackwards()
-    {
-        float q1, q2, q3;
-        q1 = 26;
-        q2 = -32;
-        q3 = -11;
-        robot->GetLeg(1)->setDesiredAngles(90 + q1, 90 + q2, 90 + q3);
-        robot->GetLeg(4)->setDesiredAngles(90 - q1, 90 - q2, 90 - q3);
-
-        q1 = 52;
-        q2 = -25;
-        q3 = 17;
-        robot->GetLeg(2)->setDesiredAngles(90 + q1, 90 - q2, 90 - q3);
-        robot->GetLeg(3)->setDesiredAngles(90 - q1, 90 + q2, 90 + q3);
-    }
-
-    void setBalanceDown()
-    {
-        float q1, q2, q3;
-        q1 = 86;
-        q2 = -80;
-        q3 = 0;
-        robot->GetLeg(1)->setDesiredAngles(90 - q1, 90 + q2, 90 + q3);
-        robot->GetLeg(4)->setDesiredAngles(90 + q1, 90 - q2, 90 - q3);
-        robot->GetLeg(2)->setDesiredAngles(90 + q1, 90 - q2, 90 - q3);
-        robot->GetLeg(3)->setDesiredAngles(90 - q1, 90 + q2, 90 + q3);
-    }
-
-    
-    void setBalanceUp()
-    {
-        float q1, q2, q3;
-        q1 = -45;
-        q2 = 45;
-        q3 = 0;
-        robot->GetLeg(1)->setDesiredAngles(90 - q1, 90 + q2, 90 + q3);
-        robot->GetLeg(4)->setDesiredAngles(90 + q1, 90 - q2, 90 - q3);
-        robot->GetLeg(2)->setDesiredAngles(90 + q1, 90 - q2, 90 - q3);
-        robot->GetLeg(3)->setDesiredAngles(90 - q1, 90 + q2, 90 + q3);
-    }
-
-    bool gotDesiredPose()
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            if (!robot->GetLeg(i + 1)->gotDesiredAngles())
-                return false;
-        }
-
-        return true;
-    }
-
-    void setSpeetTTF(float ttf)
-    {
-        for (int i = 1; i <= 4; i++)
-        {
-            robot->GetLeg(i)->setSpeedTTF(ttf);
-        }
-    }
 
     bool inServoRange(float angle)
     {
@@ -146,6 +65,9 @@ private: // functions
         result[1] = q1 * RAD_TO_DEG;
         result[2] = q2 * RAD_TO_DEG;
     }
+
+
+
 
 public:
     void update(long timeElapsed) override
